edd5ac51dde7c3425d0f8fe9be110f6698da0020
JDeoks <duginee@naver.com>
2023-04-27 20:56:15 +0900

    braille grid size fit

--- OE_SwiftUI/OE_SwiftUI/OE_SwiftUI Watch App/ContentView.swift
+++ OE_SwiftUI/OE_SwiftUI/OE_SwiftUI Watch App/ContentView.swift
@@ -12,94 +12,124 @@ import WatchKit
 
 struct ContentView: View {
     @ObservedObject var model = ViewModelWatch()
-    @State var str: String = ""
+    @State var str: String = "기본"
     @State private var crownValue = 0.0
     @State var crownIndex: Int = 0
     @State var lastCrown = 0.0
-    @State var braille2DArr: [[Int]] = [[0,0,0,0,0,0]]
+    @State var braille2DArr: [[Int]] = [[0,1,0,1,0,1]]
 
 
     var body: some View {
-        Text("값: \(str)")
-            // 변화를 감지할 변수이름에 $를 붙여 감시, 파라미터는 변화한 값
-            .onReceive(self.model.$messageText) { message in
-                self.str = message
-                if message != "" {
-                    braille2DArr = convert(str: message)
-                    print(braille2DArr)
-                }
-            }
-
-        Text("\(crownIndex): \(String(str[crownIndex]))")
-//            .font(.largeTitle)
-            .focusable()
-            // $crownValue 위치에 값 받을 변수 넣음
-            .digitalCrownRotation($crownValue) { DigitalCrownEvent in
-                // DigitalCrownEvent.offset 으로 크라운값 받기 가능
-                if crownValue > lastCrown + 20 {
-                    lastCrown = crownValue
-                    if crownIndex < braille2DArr.count - 1 {
-                        //진동
-                        crownIndex += 1
-                    }
-                    
-                }
-                else if crownValue <  lastCrown - 20 {
-                    lastCrown = crownValue
-                    if crownIndex > 0 {
-                        //진동
-                        crownIndex -= 1
+        VStack{
+            Text("값: \(str)")
+                // 변화를 감지할 변수이름에 $를 붙여 감시, 파라미터는 변화한 값
+                .onReceive(self.model.$messageText) { message in
+                    self.str = message
+                    if message != "" {
+                        braille2DArr = convert(str: message)
+                        print(braille2DArr)
                     }
                 }
-//                crownIndex = Int(DigitalCrownEvent.offset)/10
-            }
-        
-//        Text("\(braille2DArr[crownIndex].map(String.init).joined())")
 
-//        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
-//            ForEach(0..<3) { row in
-//                ForEach(0..<2) { col in
-//                    let index = row + (col * 3)
-//                    GeometryReader { geo in
-//                        Text("\(index + 1)")
-//                            .font(.largeTitle)
-//                            .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.1)
-//                            .frame(width: geo.size.width, height: geo.size.height)
-//                            .onTapGesture {
-//                                print("Tapped index: \(index + 1)")
-//                            }
+            Text("\(crownIndex): \(String(str[crownIndex]))")
+    //            .font(.largeTitle)
+                .focusable()
+                // $crownValue 위치에 값 받을 변수 넣음
+                .digitalCrownRotation($crownValue) { DigitalCrownEvent in
+                    // DigitalCrownEvent.offset 으로 크라운값 받기 가능
+                    if crownValue > lastCrown + 20 {
+                        lastCrown = crownValue
+                        if crownIndex < braille2DArr.count - 1 {
+                            //진동
+                            crownIndex += 1
+                        }
+
+                    }
+                    else if crownValue <  lastCrown - 20 {
+                        lastCrown = crownValue
+                        if crownIndex > 0 {
+                            //진동
+                            crownIndex -= 1
+                        }
+                    }
+    //                crownIndex = Int(DigitalCrownEvent.offset)/10
+                }
+            
+    //        Text("\(braille2DArr[crownIndex].map(String.init).joined())")
+
+//            LazyVGrid(columns: [
+//                GridItem(.flexible(minimum: 30, maximum: 100)), GridItem(.flexible(minimum: 30, maximum: 100))
+//            ], spacing: 10) {
+//                ForEach(0..<3) { row in
+//                    ForEach(0..<2) { col in
+//                        let index = row + (col * 3)
+//                        GeometryReader { geo in
+//                            Text("\(index + 1)")
+//                                .font(.largeTitle)
+//                                .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.1)
+//                                .frame(width: geo.size.width, height: geo.size.height)
+//                                .onTapGesture {
+//                                    print("Tapped index: \(index + 1)")
+//                                }
+//                        }
 //                    }
 //                }
 //            }
-//        }
-        
-        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
-            ForEach(0..<3) { row in
-                ForEach(0..<2) { col in
-                    let index = row + (col * 3)
-                    GeometryReader { geo in
-                        Text("\(index + 1)")
-                            .font(.largeTitle)
-                            .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.1)
-                            .frame(width: geo.size.width, height: geo.size.height)
-                            .onTapGesture {
-                                print("Tapped index: \(index + 1)")
-                            }
+            
+//            LazyVGrid(columns: [
+//                GridItem(.flexible(minimum: 30, maximum: 100)), GridItem(.flexible(minimum: 30, maximum: 100))
+//            ], spacing: 10) {
+//                ForEach(0..<3) { row in
+//                    ForEach(0..<2) { col in
+//                        let index = row + (col * 3)
+////                        GeometryReader { geo in
+////                            let width = geo.size.width / 2
+////                            let height = geo.size.height / 3
+//
+//                            Text("\(index + 1)")
+//                                .font(.largeTitle)
+////                                .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.1)
+////                                .frame(width: width, height: height)
+////                                .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
+//                                .onTapGesture {
+//                                    print("Tapped index: \(index + 1)")
+//                                }
+//                        }
+//                    }
+////                }
+//            }
+            LazyVGrid(columns: [
+                GridItem(.flexible(minimum: 10, maximum: 50)), GridItem(.flexible(minimum: 10, maximum: 50))
+            ], spacing: 10) {
+                ForEach(0..<3) { row in
+                    ForEach(0..<2) { col in
+                        let index = row + (col * 3)
+                        GeometryReader { geo in
+                            let width = geo.size.width / 2
+                            let height = geo.size.height / 3
+                            
+                            Text("\(index + 1)")
+                                .font(.largeTitle)
+                                .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.4)
+                                .frame(width: width, height: height)
+                                .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
+                                .onTapGesture {
+                                    print("Tapped index: \(index + 1)")
+                                }
+                        }
+                        .aspectRatio(1, contentMode: .fit)
                     }
                 }
             }
+
+            
+
         }
-
-        
-
-
-
-        
     }
     /// 일반 String 받아서 점자 Int arr로 반환. 입력: "Hello", 출력: [[0,1,1,0,0,0],[0,0,0,1,1,0]]
     func convert(str string: String) -> [[Int]]{
         /// 소문자로 저장된 일반 String
-        var str = string.lowercased()
+        let str = string.lowercased()
         /// 반환할 배열, ["100011", "010010"]의 형식을 갖고있음
         var returnValue: [[Int]] = []
         /// [글자: 점자] 딕셔너리
---
+++ OE_SwiftUI/OE_SwiftUI/readme.md
@@ -0,0 +1,4 @@
+폰에서 워치로 메세지 전송  
+model.$messageText의 값이 업데이트 될 때 str에 복제  
+crownIndex가 str의 인덱스 값을 벗어나지 않게 함  
+Convert 함수 추가 점자 dictionary 생성해서 일반 String -> 점자 -> Int배열로 변환
