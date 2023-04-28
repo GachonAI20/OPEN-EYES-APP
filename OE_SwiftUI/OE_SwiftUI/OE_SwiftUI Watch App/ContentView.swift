9e8ec5976b3e14dbccac6a510bf3c021fc7a2a48
JDeoks <duginee@naver.com>
2023-04-28 22:14:33 +0900

    remove label

--- OE_SwiftUI/OE_SwiftUI/OE_SwiftUI Watch App/ContentView.swift
+++ OE_SwiftUI/OE_SwiftUI/OE_SwiftUI Watch App/ContentView.swift
@@ -11,6 +11,7 @@ import WatchKit
 
 
 struct ContentView: View {
+    
     @ObservedObject var model = ViewModelWatch()
     @State var str: String = "기본"
     @State private var crownValue = 0.0
@@ -21,85 +22,10 @@ struct ContentView: View {
 
     var body: some View {
         VStack{
-            Text("값: \(str)")
-                // 변화를 감지할 변수이름에 $를 붙여 감시, 파라미터는 변화한 값
-                .onReceive(self.model.$messageText) { message in
-                    self.str = message
-                    if message != "" {
-                        braille2DArr = convert(str: message)
-                        print(braille2DArr)
-                    }
-                }
-
-            Text("\(crownIndex): \(String(str[crownIndex]))")
-    //            .font(.largeTitle)
-                .focusable()
-                // $crownValue 위치에 값 받을 변수 넣음
-                .digitalCrownRotation($crownValue) { DigitalCrownEvent in
-                    // DigitalCrownEvent.offset 으로 크라운값 받기 가능
-                    if crownValue > lastCrown + 20 {
-                        lastCrown = crownValue
-                        if crownIndex < braille2DArr.count - 1 {
-                            //진동
-                            crownIndex += 1
-                        }
-
-                    }
-                    else if crownValue <  lastCrown - 20 {
-                        lastCrown = crownValue
-                        if crownIndex > 0 {
-                            //진동
-                            crownIndex -= 1
-                        }
-                    }
-    //                crownIndex = Int(DigitalCrownEvent.offset)/10
-                }
+            Text("\(str)\(crownIndex): \(String(str[crownIndex]))")
             
-    //        Text("\(braille2DArr[crownIndex].map(String.init).joined())")
-
-//            LazyVGrid(columns: [
-//                GridItem(.flexible(minimum: 30, maximum: 100)), GridItem(.flexible(minimum: 30, maximum: 100))
-//            ], spacing: 10) {
-//                ForEach(0..<3) { row in
-//                    ForEach(0..<2) { col in
-//                        let index = row + (col * 3)
-//                        GeometryReader { geo in
-//                            Text("\(index + 1)")
-//                                .font(.largeTitle)
-//                                .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.1)
-//                                .frame(width: geo.size.width, height: geo.size.height)
-//                                .onTapGesture {
-//                                    print("Tapped index: \(index + 1)")
-//                                }
-//                        }
-//                    }
-//                }
-//            }
-            
-//            LazyVGrid(columns: [
-//                GridItem(.flexible(minimum: 30, maximum: 100)), GridItem(.flexible(minimum: 30, maximum: 100))
-//            ], spacing: 10) {
-//                ForEach(0..<3) { row in
-//                    ForEach(0..<2) { col in
-//                        let index = row + (col * 3)
-////                        GeometryReader { geo in
-////                            let width = geo.size.width / 2
-////                            let height = geo.size.height / 3
-//
-//                            Text("\(index + 1)")
-//                                .font(.largeTitle)
-////                                .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.1)
-////                                .frame(width: width, height: height)
-////                                .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
-//                                .onTapGesture {
-//                                    print("Tapped index: \(index + 1)")
-//                                }
-//                        }
-//                    }
-////                }
-//            }
             LazyVGrid(columns: [
-                GridItem(.flexible(minimum: 10, maximum: 50)), GridItem(.flexible(minimum: 10, maximum: 50))
+                GridItem(.fixed(50)), GridItem(.fixed(50))
             ], spacing: 10) {
                 ForEach(0..<3) { row in
                     ForEach(0..<2) { col in
@@ -113,9 +39,7 @@ struct ContentView: View {
                                 .opacity(braille2DArr[crownIndex][5 - index] == 1 ? 1 : 0.4)
                                 .frame(width: width, height: height)
                                 .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY)
-                                .onTapGesture {
-                                    print("Tapped index: \(index + 1)")
-                                }
+                                
                         }
                         .aspectRatio(1, contentMode: .fit)
                     }
@@ -124,6 +48,37 @@ struct ContentView: View {
 
             
 
+        }
+        // 변화를 감지할 변수이름에 $를 붙여 감시, 파라미터는 변화한 값
+        .onReceive(self.model.$messageText) { message in
+            self.str = message
+            print(message)
+            if message != "" {
+                braille2DArr = convert(str: message)
+                print(braille2DArr)
+            }
+        }
+        // 뷰에 포커스를 설정할 수 있으며, Digital Crown 회전 이벤트가 발생할 때마다 이를 감지하고 처리한다.
+        .focusable()
+        // $crownValue 위치에 값 받을 변수 넣음
+        .digitalCrownRotation($crownValue) { DigitalCrownEvent in
+            // DigitalCrownEvent.offset 으로 크라운값 받기 가능
+            if crownValue > lastCrown + 20 {
+                lastCrown = crownValue
+                if crownIndex < braille2DArr.count - 1 {
+                    //진동
+                    crownIndex += 1
+                }
+
+            }
+            else if crownValue <  lastCrown - 20 {
+                lastCrown = crownValue
+                if crownIndex > 0 {
+                    //진동
+                    crownIndex -= 1
+                }
+            }
+//                crownIndex = Int(DigitalCrownEvent.offset)/10
         }
     }
     /// 일반 String 받아서 점자 Int arr로 반환. 입력: "Hello", 출력: [[0,1,1,0,0,0],[0,0,0,1,1,0]]
@@ -184,16 +139,19 @@ struct ContentView: View {
             "⠾": [1,1,1,1,1,0], "⠿": [1,1,1,1,1,1]
         ]
         
-        // 글자 -> 점자 -> IntArr
+        // 입력받은 문자열의 각 글자를 순회하면서 점자로 변환하고,
+        // 점자를 이진 숫자 배열로 변환하여 반환할 배열에 추가
         for i in 0..<str.count {
+            // 입력받은 문자열에서 i번째 글자를 가져옴
             let char: Character = str.getChar(at: i)
-            // braille: "⠗"
+            // i번째 글자에 해당하는 점자 문자를 딕셔너리에서 찾음 braille: "⠗"
             if let braille: Character = eng2Braille[char] {
-                print(braille)
+                print(braille, terminator: " ")
+                // 점자 문자에 해당하는 이진 숫자 배열을 반환할 배열에 추가
                 returnValue.append(braille2IntArr[braille]!)
             }
         }
-        
+        // 변환된 이진 숫자 2DArr을 반환
         return returnValue
     }
     
--- OE_SwiftUI/OE_SwiftUI/OE_SwiftUI/ContentView.swift
+++ OE_SwiftUI/OE_SwiftUI/OE_SwiftUI/ContentView.swift
@@ -7,37 +7,45 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
+    // ViewModelPhone 인스턴스를 생성
     var model = ViewModelPhone()
+    
+    // reachable: 연결 상태를 나타내는 문자열 변수. 초기값은 "No"
     @State var reachable = "No"
+    
+    // messageText: 사용자가 입력할 메시지를 저장하는 문자열 변수
     @State var messageText = ""
+    
     var body: some View {
         VStack{
+            // 연결 상태를 표시
             Text("Reachable \(reachable)")
             
+            // "Update" 버튼: 클릭 시 Apple Watch와의 연결 상태를 확인하고 reachable 변수를 업데이트
             Button(action: {
-                if self.model.session.isReachable{
+                if self.model.session.isReachable {
                     self.reachable = "Yes"
-                }
-                else{
+                } else {
                     self.reachable = "No"
                 }
                 
             }) {
                 Text("Update")
             }
+            
+            // 사용자가 메시지를 입력할 수 있는 텍스트 필드
             TextField("Input your message", text: $messageText)
+            
+            // "Send Message" 버튼: 클릭 시 입력한 메시지를 Apple Watch로 전송
             Button(action: {
-                self.model.session.sendMessage(["message" : self.messageText], replyHandler: nil) { (error) in
+                self.model.session.sendMessage(["message": self.messageText], replyHandler: nil) { (error) in
                     print(error.localizedDescription)
                 }
             }) {
-            Text("Send Message")
+                Text("Send Message")
             }
         }
-        
     }
 }
 
